bca6e4bfee1822519c40da87ee8089db97b3b7db
Kanda Motohiro <kanda.motohiro@gmail.com>
2018-05-02 12:27:41 +0900

    serialize.cpp done.

--- serialize.cpp
+++ serialize.cpp
@@ -15,6 +15,8 @@ using namespace std;
 class Base {
     string s;
     int i;
+
+    // 書き込みたいメンバを列挙する。
     friend class boost::serialization::access;
     template<class Archive>
     void serialize(Archive & ar, const unsigned int version)
@@ -24,36 +26,47 @@ class Base {
     }
 public:
     Base(string a, int b) : s(a), i(b) { }
-    Base() {}
+    Base() {} // これが無いと、解凍できない。
     const string toString() { return s + to_string(i); }
 };
 
 int main(int argc, char *argv[])
 {
     string path;
+    streambuf *oldbuf = nullptr;
+
     if (argc == 3)
         path = argv[2];
-    if (argc <= 2 || strcmp(argv[1], "out") == 0) {
+
+    if (argc == 1 || (2 <= argc && strcmp(argv[1], "out") == 0)) {
         Base a{"a", 0x0a};
         Base b{"b", 0x0b};
         vector<Base> vec{a, b};
         
-        ostream os{0};
+        // if 文の中においたら、スコープが外れてアーカイブを参照して落ちた。
+        ofstream out(path);
         if (path.size())
-            os.rdbuf(ofstream(path).rdbuf());
-        else
-            os.rdbuf(cout.rdbuf());
-        boost::archive::text_oarchive oa(os);
+            // cin/cout を入れ替えるにはこうするそうな。
+            oldbuf = cout.rdbuf(out.rdbuf());
+        boost::archive::text_oarchive oa(cout);
         oa << vec;
+        if (oldbuf)
+            // 元に戻さないと、落ちる。
+            cout.rdbuf(oldbuf);
     } else {
-        istream is{0};
+        ifstream in(path);
         if (path.size())
-            is.rdbuf(ifstream(path).rdbuf());
-        else
-            is.rdbuf(cin.rdbuf());
-        boost::archive::text_iarchive ia(is);
+            oldbuf = cin.rdbuf(in.rdbuf());
+        boost::archive::text_iarchive ia(cin);
         vector<Base> vec;
         ia >> vec;
         for (Base& a: vec) { cout << a.toString() << endl; }
+        if (oldbuf)
+            cin.rdbuf(oldbuf);
     }
 }
+/*
+ $ echo -n 22 serialization::archive 14 0 0 2 0 0 0 1 a 10 1 b 11 | ./a.out in
+a10
+b11
+ */
