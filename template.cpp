be5ca3c87316ef4dc4eb19621d90d3db6d805808
Kanda Motohiro <kanda.motohiro@gmail.com>
2018-09-08 21:46:19 +0900

    template metaprogramming.

--- template.cpp
+++ template.cpp
@@ -1,84 +1,110 @@
 /*
- * 2018.6.24 kanda.motohiro@gmail.com
- * C++ 練習
+ * 2018.9.8 kanda.motohiro@gmail.com C++ 練習
  * released under https://creativecommons.org/publicdomain/zero/1.0/legalcode.ja
  */
-#include <iostream>
-#include <vector>
-using namespace std;
+//#include "base.h"
+#include <type_traits>
+#include <tuple>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
 
-#define p(a) { cout << (a) << " at line " << __LINE__ << endl; }
+class Base {
+public:
+virtual ~Base() {}
+};
 
-template<typename T> auto add(T a0, T a1) -> T
+template<typename T>
+T Double(T a)
 {
-    T sum = a0 + a1;
-    return sum;
+    static_assert(std::is_integral<T>::value);
+    //assert(is_integral<T>::value); // ランタイムにも使える。
+    return a+a;
 }
+
+template<typename T>
+void copy(T* p, const T* q, int n)
+{
+    if (std::is_pod<T>::value)
+        memcpy(p, q, n*sizeof(T));
+    else
+        for (int i=0; i!=n; i++)
+            p[i] = q[i];
+}
+
+namespace std {
+template<bool C, typename T, typename F>
+struct Conditional {
+    using type = T;
+};
+
+template<typename T, typename F>
+struct Conditional<false,T,F> {
+    using type = F;
+};
+}
+
+template<typename T>
+class K {
+public:
+    T body;
+    typename std::Conditional<std::is_pod<T>::value, int, char>::type size;
+};
+
+template<int N>
+constexpr int fac() { return N*fac<N-1>(); }
+template<>
+constexpr int fac<1>() { return 1; }
+
 
 int main()
 {
-    auto a = add<int>(1, 2);
-    p(a);
-    auto b = add(string{"aaa"}, string{"bbb"});
+{
+    std::tuple<int, std::string, Base> t{3, "hello", Base{}};
+    int i = std::get<0>(t);
+    std::string s;
+    Base b;
+    std::tie(i, s, b) = t;
+    auto [j, k, l] = t;
+}
+
+    constexpr int x5 = fac<5>();
+    K<int> kint;
+    K<Base> kbase;
+
+    printf("kint.size=%d kbase.size=%d\n", sizeof(kint.size), sizeof(kbase.size));
+
+//static_assert(std::is_pod<Base>::value);
+    constexpr int n = 32;
+    int b[n];
+    int c[n];
+    Base d[n];
+    Base e[n];
+
+    copy(b, c, n);
+    copy(d, e, n);
+
+    (void)Double<int>(1);
+    /*
     p(b);
-    //auto c = add(vector<int>{1}, vector<int>{2});
+    float c = 3.14;
+    float d = Double<float>(c);
+    p(d);
+    Base e;
+    auto b = Double<Base>(Base()); // error
+    */
 }
 #if 0
-(gdb) b add
-Function "add" not defined.
-Make breakpoint pending on future shared library load? (y or [n]) n
-(gdb) b _Z3addIiET_S0_S0_
-Breakpoint 1 at 0x8048d27: file template.cpp, line 14.
-(gdb) b _Z3addINSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEET_S6_S6_
-Breakpoint 2 at 0x8048d3d: file template.cpp, line 14.
-(gdb) run
+template.cpp: In instantiation of ‘T Double(T) [with T = Base]’:
+template.cpp:20:28:   required from here
+template.cpp:9:11: エラー: no match for ‘operator+’ (operand types are ‘Base’ and ‘Base’)
+ { return a+a; }
+          ~^~
+template.cpp: In instantiation of ‘T Double(T) [with T = float]’:
+template.cpp:20:30:   required from here
+template.cpp:10:5: エラー: static assertion failed
+     static_assert(is_integral<T>::value);
+     ^~~~~~~~~~~~~
 
-Breakpoint 1, add<int> (a0=1, a1=2) at template.cpp:14
-14	    T sum = a0 + a1;
-(gdb) n
-15	    return sum;
-(gdb) bt
-#0  add<int> (a0=1, a1=2) at template.cpp:15
-#1  0x08048b09 in main () at template.cpp:20
-(gdb) c
-Continuing.
-3 at line 21
 
-Breakpoint 2, add<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > > (a0="aaa", a1="bbb") at template.cpp:14
-14	    T sum = a0 + a1;
-(gdb) bt
-#0  add<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > > (a0="aaa", a1="bbb") at template.cpp:14
-#1  0x08048bb5 in main () at template.cpp:22
-(gdb) c
-Continuing.
-aaabbb at line 23
-
-$ nm --demangle a.out | grep add
-08048d37 W std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > add<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >)
-08048d21 W int add<int>(int, int)
-
-(gdb) b *0x08048d21
-Breakpoint 1 at 0x8048d21: file template.cpp, line 12.
-(gdb) run
-
-Breakpoint 1, add<int> (a0=1, a1=2) at template.cpp:12
-12	template<typename T> auto add(T a0, T a1) -> T
-(gdb) n
-14	    T sum = a0 + a1;
-(gdb) b *0x08048d37
-Breakpoint 2 at 0x8048d37: file template.cpp, line 12.
-(gdb) c
-Continuing.
-3 at line 21
-
-Breakpoint 2, add<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > > (a0="aaa", a1="bbb") at template.cpp:12
-12	template<typename T> auto add(T a0, T a1) -> T
-
-(gdb) b add<int>
-Breakpoint 1 at 0x8048d27: file template.cpp, line 14.
-賢い。
-
-(gdb) b add<std::string>
-Function "add<std::string>" not defined.
-basic_string なんとか、と書かないといけないのだろう。
 #endif
